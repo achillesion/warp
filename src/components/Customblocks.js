f529dd615459032a19aea8ae463e8a687572529f
Ahmad Bashir <achillesion@gmail.com>
2022-11-05 02:37:09 +0500

    first commit

--- src/components/Customblocks.js
+++ src/components/Customblocks.js
@@ -4,6 +4,7 @@ import blockvectorimg from '../assets/blockvector.png';
 
 const Customblocks = () => {
      return (
+          <>
           <div className='customblockswrap'>
                <div className='container '>
                     <div className='content-box'>
@@ -56,6 +57,54 @@ const Customblocks = () => {
                </div>
 
           </div>
+                <div className='customblockswrap' id="mobilecustomblockswrap" >
+                <div className='container '>
+                     <div className='content-box'>
+                          <img className='connectwalltbtn' alt='' />
+                          <div><h5></h5></div>
+ 
+                          <div><h5>10 %</h5><p>Earn a fixed 10% weekly ROI</p></div>
+                          <div><h5></h5></div>
+
+                          <div><h5>1</h5><p>Minimum Deposit</p></div>
+                          <div><h5></h5></div>
+
+                          <div><h5>730 %</h5><p>Max ROI of deposits with Compounds</p></div>
+                          <div><h5></h5></div>
+
+                          <div><h5>5x</h5><p>You can compound upto</p></div>
+                          <div><h5></h5></div>
+
+                          <div><h5>10 %</h5><p>Every deposit and collect has
+                               base fee/  5% TVL, 5% Dev</p></div>
+                          <div><h5></h5></div>
+                          
+                     </div>
+                     {/* Second row */}
+                     <div className='content-box' id='content-boxx'>
+                          <img className='connectwalltbtn' alt='' />
+                          <div><h5></h5></div>
+ 
+                          <div ><img className='max-img' src={blockvectorimg} /><p>Maximum Deposit</p></div>
+                          <div><h5></h5></div>
+
+                          <div><h5>730 %</h5><p>APR</p></div>
+                          <div><h5></h5></div>
+ 
+                          <div><h5>365 %</h5><p>Maximum ROI</p></div>
+                          <div><h5></h5></div>
+ 
+                          <div><h5>3%</h5><p>Refferal Commission</p></div>
+                          <div><h5></h5></div>
+
+                          <div><h5>5th Day</h5><p>Compound or collect
+                               every</p></div>
+                          <div><h5></h5></div>
+                     </div>
+                </div>
+ 
+           </div>
+           </>
      )
 }
 
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,14 +1,73 @@
 import React from 'react'
-import Herosection from '../components/Herosection'
-import Notice from '../components/Notice'
-import Customblocks from '../components/Customblocks'
+import noticee from '../assets/Important notice.png'
+import wallet from '../assets/wallet.png'
+import "./dashboard.css"
 
-const Dashboard = () => {
+function Dashboard() {
   return (
     <>
-    < Herosection />
-    < Notice />
-    < Customblocks />
+      <div className='parentdisclaimer'>
+        <div className='container'>
+          {/* leftside */}
+          <div className='leftside'>
+            <div className='topdisclaimerwrap'>
+              <div className='content-box'>
+                <img className='connectwalltbtn' src={noticee} alt='' />
+                <h3>Disclaimer</h3>
+              </div>
+              <div>
+                <p>Funds that are used to initially hire farmers (including re-hire) cannot be withdrawn, however your farmers will indefinitely work and grow crops for you. Please use the application at your own risk.</p>
+              </div>
+            </div>
+            <div className='bottomdisclaimerwrap'>
+              <div className='leftimepool'>
+                <div className='hirwall'>
+                  <div className='hiring'>
+                    <p>Hiring Example</p>
+                    <p>100 WRAP = 0 Farmers</p>
+                    <p>Daily Yeild: 0 WRAP</p>
+                  </div>
+                  <div className='hiringright'>
+                    <h3><img src={wallet} />   Wallet</h3>
+                    <p>100 WRAP = 0 Farmers</p>
+                    <p>Daily Yeild: 0 WRAP</p>
+                  </div>
+                </div>
+              </div>
+              <div className='rightimepool'>sell</div>
+            </div>
+          </div>
+          {/* rightside */}
+          <div className='rightside'>
+            <div className='content-box'>
+              <div className='darkbluebox'>
+                <p>Initial Deposit</p>
+                <h4>0 WARP</h4>
+              </div>
+              <div className='darkbluebox'>
+                <p>Total Deposit</p>
+                <h4>0 WARP</h4>
+              </div>
+              <div className='darkbluebox'>
+                <p>Withdrawn</p>
+                <h4>0 WARP</h4>
+              </div>
+              <div className='darkbluebox'>
+                <p>Refferals (0)</p>
+                <h4>0 WARP</h4>
+              </div>
+              <div className='darkbluebox'>
+                <p>Total BUSD Locked</p>
+                <h4>0 WARP</h4>
+              </div>
+              <div className='darkbluebox'>
+                <p>Total Deposit Count</p>
+                <h4>0 WARP</h4>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
