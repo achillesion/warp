b6816e5d35d8419b5d051f9570cb673cd8ca4e96
Imranmazhar323 <117320989+Imranmazhar323@users.noreply.github.com>
2022-11-09 03:19:00 +0500

    faq and swaps screen for both desktop and mobile has been done ad faq is not added in menu but it's file is added in the  project

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ const App = () => {
           <Route exact path="/" element={<Home />} />
           <Route path="/Dashboard" element={<Dashboard />}/>
           <Route path="/coin-flipper" element={<Coinflipping />}/>
-          <Route path="/contact-us" element={<Faq />}/>
+          {/* <Route path="/contact-us" element={<Faq />}/> */}
           <Route path="/swaps" element={<Swaps />}/>
         </Routes>
         < Footer />
