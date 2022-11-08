86d3b65f6958d4fca10a2135ce1de3bf93318c97
Imranmazhar323 <117320989+Imranmazhar323@users.noreply.github.com>
2022-11-09 03:51:10 +0500

    11-9-2022 latest commit faq and swaps screens is added

--- src/App.js
+++ src/App.js
@@ -3,8 +3,11 @@ import Home from "./pages/Home";
 import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
 import Menu from "./components/Menu";
 import Dashboard from "./components/Dashboard";
+import Coinflipping from "./components/Coinflipping";
 import "./index.css";
 import Footer from "./components/Footer";
+import Faq from "./components/Faq";
+import Swaps from "./components/Swaps";
 
 const App = () => {
   return (
@@ -14,6 +17,9 @@ const App = () => {
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route path="/Dashboard" element={<Dashboard />}/>
+          <Route path="/coin-flipper" element={<Coinflipping />}/>
+          {/* <Route path="/contact-us" element={<Faq />}/> */}
+          <Route path="/swaps" element={<Swaps />}/>
         </Routes>
         < Footer />
       </div>
